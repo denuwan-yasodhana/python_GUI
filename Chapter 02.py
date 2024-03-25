19e6ce6e8374f1ccd2732557c7dc3f169a9f125f
Denuwan Yasodhana <Denuwan_Yasodhana77@outlook.com>
2024-03-25 16:44:36 +0530

    Add files via upload

---
+++ Chapter 02.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from tkinter import *
+
+root = Tk()
+
+#Create label
+myLabel1 = Label(root, text = "Hello World")
+myLabel2 = Label(root, text = "My name is Denuwan")
+myLabel3 = Label(root, text = "Age is 26")
+
+myLabel1.grid(row=0, column=0)
+myLabel2.grid(row=1, column=0)
+myLabel3.grid(row=2, column=1)
+
+root.mainloop()
