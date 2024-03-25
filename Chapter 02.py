8df53b66cd6803486288348c68348e07f56966a0
Denuwan Yasodhana <Denuwan_Yasodhana77@outlook.com>
2024-03-25 16:59:28 +0530

    Update Chapter 02.py

--- Chapter 02.py
+++ Chapter 02.py
@@ -6,7 +6,7 @@ root = Tk()
 #Create label
 myLabel1 = Label(root, text = "Hello World")
 myLabel2 = Label(root, text = "My name is Denuwan")
-myLabel3 = Label(root, text = "Age is 26")
+myLabel3 = Label(root, text = "Age is 26") # can empty like this"   " 
 
 myLabel1.grid(row=0, column=0)
 myLabel2.grid(row=1, column=0)
