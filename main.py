46e9d1cbade86c842d73130f7b21c6f1794daca8
Denuwan Yasodhana <Denuwan_Yasodhana77@outlook.com>
2024-03-25 16:24:22 +0530

    Create Simple Graphical User Interfaces With Python And TKinter

---
+++ main.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import *
+
+root = Tk()
+
+#Create label
+myLabel = Label(root, text = "Hello World")
+
+#Showing it onto screen
+myLabel.pack()
+
+root.mainloop()
+
